febbe9ac9ed268d19c9d40902f74816bfe42cd94
rafaelmaframg <69654497+rafaelmaframg@users.noreply.github.com>
2020-11-13 11:34:20 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from CONEXAO import criar_conexao, fechar_conexao
+
+def insere_usuario(con, nome, email, senha ):
+    cursor = con.cursor()
+    sql = 'INSERT INTO usuario (nome, email, senha) values (%s, %s, %s)'
+    valores = (nome, email, senha)
+    cursor.execute(sql, valores)
+    cursor.close()
+    con.commit()
+
+def select_todos_usuarios(con):
+    cursor = con.cursor()
+    sql = 'SELECT id, nome, email FROM usuario'
+    cursor.execute(sql)
+
+    for (id, nome, email) in cursor:
+        print(id, nome, email)
+    
+    cursor.close()
+
+def main():
+    con = criar_conexao('localhost','root','','youtube')
+    insere_usuario(con,'olamundo','rafael@ooooo.com.br','olamundo')
+    select_todos_usuarios(con)
+    fechar_conexao(con)
+
+if __name__ == '__main__':
+    main()
